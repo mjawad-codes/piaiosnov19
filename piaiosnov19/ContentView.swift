6401678cc933dfff2457b9a91ad21bcdc778fe73
Muhammad Jawad <jawadbqa@gmail.com>
2025-11-24 11:49:50 +0100

    another fruit

--- piaiosnov19/ContentView.swift
+++ piaiosnov19/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Banan")
+            Text("Kiwi")
             Text("Hello, world!")
         }
         .padding()
